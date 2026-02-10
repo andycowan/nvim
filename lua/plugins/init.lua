1dd56e116f02b6d02e9994bbd6aac53e4cd52ed0
Andrew Cowan <andrew.cowan@e-track.com>
2026-02-10 00:48:14 +0000

    better tree config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,6 +13,36 @@ return {
     end,
   },
 
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = {
+      git = {
+        enable = true,
+        ignore = false,  -- Show git-ignored files
+        timeout = 400,   -- Git status update timeout
+      },
+      renderer = {
+        highlight_git = true,
+        icons = {
+          show = {
+            git = true,
+          },
+        },
+      },
+      update_focused_file = {
+        enable = true,
+        update_root = false,
+      },
+      -- Auto-refresh on various events
+      view = {
+        adaptive_size = false,
+      },
+      filesystem_watchers = {
+        enable = true,
+        debounce_delay = 50,
+      },
+    },
+  },
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
